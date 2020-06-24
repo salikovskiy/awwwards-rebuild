61165252352563cd10c3e54905b93ca0d34bb78d
salikovskiy <ssalikovskiy@gmail.com>
2020-06-24 15:58:29 +0300

    Canvas painting effect + HomepageBaner done

---
+++ src/components/homePage/HomeBanner.js
@@ -0,0 +1,100 @@
+import React, { useRef, useEffect } from 'react'
+import { Banner, Video, BannerTitle, Canvas, Headline } from '../../styles/homeStyles'
+
+// Context
+import { useGlobalStateContext } from '../../context/globalContext'
+
+//Custom hook
+import useWindowSize from '../../hooks/useWindowSize'
+
+
+const HomeBanner = ({ onCursor }) => {
+    const size = useWindowSize()
+    const { currentTheme } = useGlobalStateContext()
+    let canvas = useRef(null)
+
+    useEffect(() => {
+        let renderingElement = canvas.current
+        let drawingElement = renderingElement.cloneNode()
+        let drawingCtx = drawingElement.getContext("2d")
+        let renderingCtx = renderingElement.getContext("2d")
+        let lastX
+        let lastY
+        let moving = false
+
+        renderingCtx.globalCompositeOperation = "source-over"
+        renderingCtx.fillStyle = currentTheme === "dark" ? "#000000" : "#ffffff"
+        renderingCtx.fillRect(0, 0, size.width, size.height)
+
+        renderingElement.addEventListener("mouseover", ev => {
+            moving = true
+            lastX = ev.pageX - renderingElement.offsetLeft
+            lastY = ev.pageY - renderingElement.offsetTop
+        })
+
+        renderingElement.addEventListener("mouseup", ev => {
+            moving = false
+            lastX = ev.pageX - renderingElement.offsetLeft
+            lastY = ev.pageY - renderingElement.offsetTop
+        })
+
+
+        renderingElement.addEventListener("mousemove", ev => {
+            if (moving) {
+                drawingCtx.globalCompositeOperation = "source-over"
+                renderingCtx.globalCompositeOperation = "destination-out"
+                let currentX = ev.pageX - renderingElement.offsetLeft
+                let currentY = ev.pageY - renderingElement.offsetTop
+                drawingCtx.lineJoin = "round"
+                drawingCtx.moveTo(lastX, lastY)
+                drawingCtx.lineTo(currentX, currentY)
+                drawingCtx.closePath()
+                drawingCtx.lineWidth = 120
+                drawingCtx.stroke()
+                lastX = currentX
+                lastY = currentY
+                renderingCtx.drawImage(drawingElement, 0, 0)
+            }
+        })
+    }, [currentTheme])
+
+    const parent = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                staggerChildren: 0.2,
+            }
+        }
+    }
+    const child = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                duration: 1,
+                ease: [0.6, 0.05, -0.01, 0.9]
+            }
+        }
+    }
+
+    return (
+        <Banner>
+            <Video>
+                <video height='100%' width="100%" loop autoPlay muted src={require('../../assets/video/video.mp4')} />
+            </Video>
+            <Canvas
+                height={size.height} width={size.width}
+                ref={canvas}
+                onMouseEnter={() => onCursor('hovered')}
+                onMouseLeave={onCursor}
+            />
+            <BannerTitle variants={parent} initial="initial" animate="animate">
+                <Headline variants={child}>DIG</Headline>
+                <Headline variants={child}>DEEP</Headline>
+            </BannerTitle>
+        </Banner>
+    )
+}
+
+export default HomeBanner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
 
+//Components
+import HomeBanner from "../components/homePage/HomeBanner"
+// Context
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+
 const IndexPage = props => {
-  return <Layout>Hey guys hope you are ready to learn!</Layout>
+  const { currentTheme, cursorStyles } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
+    dispatch({ type: "CURSOR_TYPE", cursorType: cursorType })
+  }
+
+  return (
+    <Layout>
+      <HomeBanner onCursor={onCursor} />
+    </Layout>
+  )
 }
 
 export default IndexPage
